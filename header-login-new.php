8ef2e4d2ccc5887384e4d652adbe033c529530f2
syedakhter <dev.syed.akhter@gmail.com>
2022-01-21 15:09:54 -0500

    login file changes

---
+++ header-login-new.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avocado</title>
+    <link rel="stylesheet" href="css/style_new.css">
+    <link rel="stylesheet" href="css/responsive_new.css">
+</head>
+
+<body>
+<!-- header sec start -->
+<header>
+    <div class="container">
+        <div class="header-inner">
+            <a class="logo" href="#"><img src="images/logo.png" alt="logo"></a>
+            <img class="line3" src="images/line2.png" alt="line2">
+            <a class="avo" href="#"><img src="images/avo.png" alt="avo"></a>
+            <a class="avo-mobile" href="#"><img src="images/avo-mobile.png" alt="avo"></a>
+        </div>
+        <img class="line1" src="images/line1.png" alt="line1">
+        <img class="line2" src="images/line1.png" alt="line1">
+    </div>
+</header>
